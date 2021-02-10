5d8797127566ba65080aca50a029e947d9a8f71a
Vinicius Rocha <viniciusr6@gmail.com>
2021-02-10 18:15:04 -0300

    continuacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
+
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
-  return <h1>Twitter Clone</h1>
+  return (
+    <>
+      <Layout />
+
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
